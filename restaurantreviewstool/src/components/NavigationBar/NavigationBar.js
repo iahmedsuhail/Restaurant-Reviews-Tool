423e74eca979c7b9e7851582558e9ddeb0178ced
jason-mak <39265842+jason-mak@users.noreply.github.com>
2020-05-23 16:50:21 +1200

    Add Back button
    - Added a back button that functions that navigates to the previous page

--- restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
+++ restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Logo from '../Logo/Logo.js'
 
 const NavigationBar = () => {
+    const history = useHistory();
+
     return(
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <Logo/> {/* a logo for the RestrauntReviewsTool */}
+            
             <div>
+                <div className='f3 dim black pa2 pointer'>
+                    <div onClick={() => history.goBack()} > 
+                        Back
+                    </div>
+                </div>
+
                 <div className='f3 dim black pa2 pointer'>
                     <Link className="no-underline" to='/'>
                         <p>Home</p>
