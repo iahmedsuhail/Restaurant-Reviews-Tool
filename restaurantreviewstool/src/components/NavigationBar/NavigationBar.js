821bd9149c313ed7feb69b8d81da9e2854620b40
Ahmed Suhail <iahmedsuhail@gmail.com>
2020-05-24 20:25:39 +1200

    Style the NavigationBar

--- restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
+++ restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import Logo from '../Logo/Logo.js';
+import './NavigationBar.css'
 
 const NavigationBar = () => {
     const history = useHistory();
-
     return(
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-            <Logo/> {/* a logo for the RestrauntReviewsTool */}
-            <div className='f3 dim black pa2 pointer'>
-            <button className="grow f4 ph3 pv2 dib white bg-light-purple" onClick={() => history.goBack()}>
-                        Back
-                    </button>
-            </div>
-                <div className='f3 dim black pa2 pointer'>
-       
-                    <Link className="no-underline" style={{'fontWeight' : 'bold'}} to='/'>
-                        <p>Home</p>
-                    </Link>
-                    <Link className="no-underline" style={{'fontWeight' : 'bold'}} to='/comparecomponent'>
-                        <p>Compare reviews</p>
-                    </Link>
+        <div className="flex justify-between-ns">
+            <Logo /> {/* a logo for the RestrauntReviewsTool */}
+        
+            <div className='f3 dim black pa4 items-end'>
+                <div className="pa3 measure sunflower-light black pointer" onClick={() => history.goBack()}>
+                    Back
+                <Link className="no-underline solid pa4 black" to='/'>
+                    Home
+                </Link>
+                <Link className="no-underline black" to='/comparecomponent'>
+                    Compare Reviews
+                </Link>
                 </div>
-            
+            </div>
         </div>
     );
 }
