356cbc4c69e27068292ea31c70c13c555ebb8af5
Aleksander Konnerup <Alek.k@hotmail.com>
2020-05-24 18:51:54 +1200

    - Fixed navigation

--- restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
+++ restaurantreviewstool/src/components/NavigationBar/NavigationBar.js
@@ -8,18 +8,17 @@ const NavigationBar = () => {
     return(
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <Logo/> {/* a logo for the RestrauntReviewsTool */}
-
+            <div className='f3 dim black pa2 pointer'>
+            <button className="grow f4 ph3 pv2 dib white bg-light-purple" onClick={() => history.goBack()}>
+                        Back
+                    </button>
+            </div>
                 <div className='f3 dim black pa2 pointer'>
-                    <Link className="no-underline" onClick={() => history.goBack()}>
-                        <p>Back</p>
-                    </Link>
-                </div>
-                
-                <div className='f3 dim black pa2 pointer'>
-                    <Link className="no-underline" to='/'>
+       
+                    <Link className="no-underline" style={{'fontWeight' : 'bold'}} to='/'>
                         <p>Home</p>
                     </Link>
-                    <Link className="no-underline" to='/comparecomponent'>
+                    <Link className="no-underline" style={{'fontWeight' : 'bold'}} to='/comparecomponent'>
                         <p>Compare reviews</p>
                     </Link>
                 </div>
