08db554a8c534894be12c12fe6e62b7d571474cf
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-07-02 09:38:18 +1200

    Deploy site

---
+++ build/precache-manifest.072b68fd2d705a5f0c77e6573860a3b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda15701df1d4900b8092f8cb140dee7",
+    "url": "/RestaurantReviewsTool/index.html"
+  },
+  {
+    "revision": "bbbad017604c5e2bf6e9",
+    "url": "/RestaurantReviewsTool/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "dae94d871fabf586e030",
+    "url": "/RestaurantReviewsTool/static/css/main.629064fb.chunk.css"
+  },
+  {
+    "revision": "bbbad017604c5e2bf6e9",
+    "url": "/RestaurantReviewsTool/static/js/2.5ed425fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RestaurantReviewsTool/static/js/2.5ed425fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae94d871fabf586e030",
+    "url": "/RestaurantReviewsTool/static/js/main.f8a9a06d.chunk.js"
+  },
+  {
+    "revision": "1879000fee12f36d0827",
+    "url": "/RestaurantReviewsTool/static/js/runtime-main.b876752f.js"
+  },
+  {
+    "revision": "126c7bd9fad62b917a202553f0808c73",
+    "url": "/RestaurantReviewsTool/static/media/Sunflower-Light.126c7bd9.woff"
+  },
+  {
+    "revision": "c30ae2b344384318b9f418c45be2ed1a",
+    "url": "/RestaurantReviewsTool/static/media/Sunflower-Medium.c30ae2b3.woff"
+  }
+]);
