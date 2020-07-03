4d38806ae546346a15d9be93f07df3044c6d7871
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-07-03 15:17:08 +1200

    Fix NavBar colours

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -6,9 +6,9 @@ const NavigationBar = () => {
     const history = useHistory();
     return(
             <div style={{display: 'flex', justifyContent: 'flex-end'}} className="pa2">
-                <div className="measure sunflower-light black pointer pa3" onClick={() => history.goBack()}> Back </div>
-                    <Link className="no-underline pa3 sunflower-light black" to='/'> Home </Link>
-                    <Link className="no-underline black pa3 sunflower-light black" to='/comparecomponent'> Compare Reviews </Link>
+                <div className="measure sunflower-medium black pointer pa3" onClick={() => history.goBack()}> Back </div>
+                    <Link className="no-underline pa3 sunflower-medium black" to='/'> Home </Link>
+                    <Link className="no-underline black pa3 sunflower-medium black" to='/comparecomponent'> Compare Reviews </Link>
             </div>
             
     );
