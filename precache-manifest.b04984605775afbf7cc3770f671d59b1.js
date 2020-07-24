d14f1752888668575c6b2b4b35f04a9865c2683e
iahmedsuhail <iahmedsuhail@gmail.com>
2020-07-24 05:54:51 +0000

    Deploying to gh-pages from  @ 2e65be0d79a521fe668669d2f5a2f71db18be89b 🚀

---
+++ precache-manifest.b04984605775afbf7cc3770f671d59b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e942e9440863a0a1358cddd54a587edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4016450cecaf3040f05",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "0e560940963952a5203f",
+    "url": "/static/css/main.30b9994e.chunk.css"
+  },
+  {
+    "revision": "a4016450cecaf3040f05",
+    "url": "/static/js/2.ce42cd47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ce42cd47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e560940963952a5203f",
+    "url": "/static/js/main.84848889.chunk.js"
+  },
+  {
+    "revision": "7acc12ea957eb30c0b41",
+    "url": "/static/js/runtime-main.c3869bdb.js"
+  },
+  {
+    "revision": "126c7bd9fad62b917a202553f0808c73",
+    "url": "/static/media/Sunflower-Light.126c7bd9.woff"
+  },
+  {
+    "revision": "c30ae2b344384318b9f418c45be2ed1a",
+    "url": "/static/media/Sunflower-Medium.c30ae2b3.woff"
+  }
+]);
